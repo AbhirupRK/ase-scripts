c5ea4708d9ada383df8999613a9756462ccba0ad
Abhirup Roy Karmakar <abhiruprk@gmail.com>
2025-03-19 17:27:34 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Scripts for Handling Crystal Structures
+# ASE scripts for specific use cases
+
+### make-cubic.py
+
+Transforms non-cubic primitive cell to a cubic cell. Useful for creating hetero-structures.
--- make-cubic.py
+++ make-cubic.py
@@ -1,24 +1,26 @@
-#%% Convert non-cubic primitive cell to cubic cell
+#%% Transform non-cubic primitive cell to cubic cell
+
 from ase.io import read, write
 from ase.build import make_supercell, find_optimal_cell_shape
 
 prim_cell = read("POSCAR", format='vasp')
-max_size = 10
+max_size = 10       # Maximum size of supercell for searching cubic cell
 
-cubic_cell = 0
 for i in range(max_size):
     P = find_optimal_cell_shape(prim_cell.cell, i+1, 'sc')
     new_cell = make_supercell(prim_cell, P)
     angles = new_cell.cell.angles()
     if 89<=angles[0]<=91 and 89<=angles[1]<=91 and 89<=angles[2]<=91:
-        cubic_cell = new_cell
         print("Supercell size:", i+1)
-        print("Cubic cell lengths:", cubic_cell.cell.lengths())
+        print("Transformation matrix:\n", P)
+        print("Cubic cell lengths:", new_cell.cell.lengths())
         break
+    else:
+        new_cell = None
 
-if cubic_cell==0:
-    print("Could not transform to cubic cell ! \nYou can try increasing 'max_size'.")
+if new_cell==None:
+    print("Could not transform to cubic cell ! \nYou may try increasing 'max_size'.")
 else:
-    write("cubic_cell.vasp", cubic_cell, format='vasp')
+    write("cubic_cell.vasp", new_cell, format='vasp')
 
 #%%
